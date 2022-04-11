24a473c3e1fabddbd8370fae018d99b9ff6f18b0
HuynhHoangPhuc <phuchoang.huynh.207@gmail.com>
2022-04-11 23:03:36 +0700

    Test List.h

--- MatchingGame.cpp
+++ MatchingGame.cpp
@@ -1,24 +1,36 @@
-#include "GamePlay.h"
-#include <exception>
+//#include "GamePlay.h"
+//#include <exception>
+//
+//int main() {
+//    GamePlay::Awake();
+//
+//    while (true) {
+//        GamePlay::Start();
+//
+//        GamePlay::Update();
+//
+//        try {
+//            GamePlay::FixedUpdate();
+//        } catch (std::exception &e) {
+//            break;
+//        }
+//
+//        if (GamePlay::isChange) {
+//            GamePlay::LateUpdate();
+//        }
+//    }
+//
+//    return 0;
+//}
+
+#include <iostream>
+#include "Utility/Collections/List/List.h"
 
 int main() {
-    GamePlay::Awake();
-
-    while (true) {
-        GamePlay::Start();
-
-        GamePlay::Update();
-
-        try {
-            GamePlay::FixedUpdate();
-        } catch (std::exception &e) {
-            break;
-        }
-
-        if (GamePlay::isChange) {
-            GamePlay::LateUpdate();
-        }
+    auto *list = new List<int>;
+    for (int i = 0; i < 10; i++) {
+        list->push_back(i);
     }
-
+    std::cout << list->index_of(5) << std::endl;
     return 0;
 }
